95bb94c6b850e316995c41c3509e8f397882489d
almokoushik <mottaki567@gmail.com>
2025-10-01 19:26:46 +0600

    merging backend frontend and dashboard

--- src/pages/Doctors/DoctorProfile/DoctorProfile.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import SingleDoctor from "../../Home/SingleDoctor/SingleDoctor";
-
-const DoctorProfile = ({ doctor }) => {
-  return <SingleDoctor doctor={doctor}></SingleDoctor>;
-};
-
-export default DoctorProfile;
